5148d9595f516eee1534b8df8ff5ca05a8e9e36f
Cristiano <cristianovgoulart@gmail.com>
2023-05-18 16:24:34 -0300

    Add existing file

---
+++ aula-09.js
@@ -0,0 +1,11 @@
+
+function soma(item1, item2) {
+    return item1 + item2;
+}
+
+function subtrai(item1, item2) {
+    return item1 - item2;
+}
+
+console.log('Soma: ', soma(5, 6));
+console.log('Soma: ', subtrai(5, 6));
