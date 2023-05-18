a5e2baaf355d9522ef6e5cec0c4d23d26b6830bf
Cristiano <cristianovgoulart@gmail.com>
2023-05-18 13:59:31 -0300

    Add existing file

---
+++ aula-02.js
@@ -0,0 +1,8 @@
+const numero1 = 1; //constante (nunca muda)
+let numero2 = 2; //variavel
+
+if(numero1 === 1) {
+    numero2 = 6
+}
+
+console.log('Resultado:', numero2)
---
+++ aula-03.js
@@ -0,0 +1,12 @@
+const idade = 18;
+const nome = 'Cristiano';
+
+if(idade >= 18 && nome === 'Cristiano') {
+    console.log('ENTROU!')
+} else {
+    console.log('NÃO ENTROU!')
+}
+
+// =    Atribuição
+// ==   Comparativo (compara e pode aceitar string como numero)
+// ===  Comparativo mais preciso
