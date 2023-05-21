2ae17a6991704d8939a3ee125bffc44dbfa81c1c
Cristiano <cristianovgoulart@gmail.com>
2023-05-21 11:34:26 -0300

    Add existing file

---
+++ aula-11.js
@@ -0,0 +1,12 @@
+// + SOMA
+console.log('Soma (10 + 1):', 10 + 1);
+// - SUBTRAÇÃO
+console.log('Subtração (10 - 1):', 10 - 1);
+// / DIVISÃO
+console.log('Divisão (10 / 2):', 10 / 2);
+// * MULTIPLICAÇÃO
+console.log('Multiplicação (5 * 2):', 5 * 2);
+// % RESTO
+console.log('Resto (11 % 2):', 11 % 2);
+// ** ELEVADO
+console.log('Elevado (5² **):', 5 ** 2);
