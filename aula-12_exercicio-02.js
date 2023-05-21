21d1cbc97b3b582dbde6e6000d7bc07508017e93
Cristiano <cristianovgoulart@gmail.com>
2023-05-21 12:51:35 -0300

    Add existing file

---
+++ aula-12_exercicio-02.js
@@ -0,0 +1,16 @@
+const QUANTIDADE = 6;
+
+function numerosImpares(valor) {
+    let contar = 0;
+    let novoValor = valor;
+
+    while(contar < QUANTIDADE) {
+        if (novoValor % 2 !== 0) { //Melhor forma de verificar um numero impar -> numero % 2 !== 0
+            console.log(novoValor);
+            contar++;
+        }
+        novoValor++;
+    }
+}
+
+numerosImpares(8);
