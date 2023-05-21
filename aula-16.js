d7f32cc4bf3b7f6be0730f32a829d404f5571be8
Cristiano <cristianovgoulart@gmail.com>
2023-05-21 14:43:15 -0300

    Add existing file

---
+++ aula-16.js
@@ -0,0 +1,10 @@
+// VARIAVEIS IMUTAVEIS
+const numero = 4324;
+const stringA = 'casa';
+const vOuF = true; //false
+
+const arrayTest = [123, 'bola', 50];
+
+arrayTest.push('loja', 100);
+
+console.log(arrayTest);
