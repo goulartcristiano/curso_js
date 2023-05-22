8724e76a3912b8b5840a91d37fc3516c8dea14e4
Cristiano <cristianovgoulart@gmail.com>
2023-05-22 16:54:22 -0300

    Add existing file

---
+++ aula-23.js
@@ -0,0 +1,13 @@
+//Função recursiva -> função chama ela mesma para fazer um calculo
+const fatorial = (valor) => {
+    if (valor === 0 || valor === 1) {
+        return 1;
+    }
+    return valor * fatorial(valor -1);
+    // 4 * fatorial 3
+    // 3 * fatorial 2
+    // 2 * fatorial 1
+
+}
+
+console.log(fatorial(4));
