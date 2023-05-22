3241357088afd5abb3c3d07c6e62a27037cb732f
Cristiano <cristianovgoulart@gmail.com>
2023-05-22 14:43:17 -0300

    Add existing file

---
+++ aula-19.js
@@ -0,0 +1,14 @@
+
+function calcular() {
+    console.log('Passou 1!')
+}
+
+const calcular2 = function(){
+    console.log('Passou 2!')
+}
+//ECMAScript
+const calcular3 = () => console.log('Passou 3!')
+
+calcular();
+calcular2();
+calcular3();
